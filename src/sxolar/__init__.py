346947446d92ce38280ab471a1b67982f623f3af
Jim Kennington <jameswkennington@gmail.com>
2024-12-22 16:25:12 -0500

    flatten

---
+++ src/sxolar/__init__.py
@@ -0,0 +1,3 @@
+# Flatten package
+from sxolar.api.search import Query, Author, Title, Category, Abstract, All
+from sxolar.api import arxiv
--- tests/test_arxiv.py
+++ tests/test_arxiv.py
@@ -9,9 +9,6 @@ from sxolar.api import arxiv
 class TestArxivAPI:
     """Test the arxiv API"""
 
-    def test_get_and_parse(self):
-        """Test the get_and_parse function"""
-
     def test_query(self):
         """Test the query function,
 
